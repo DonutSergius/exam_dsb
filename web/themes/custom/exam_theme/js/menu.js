0fc1f189b61d8dc02651b54b5c3600f72ae2359c
DonutSergius <popov.serg.2003.09@gmail.com>
2024-03-19 18:12:18 +0200

    Created style to Service, Project, About, Contact

--- web/themes/custom/exam_theme/js/menu.js
+++ web/themes/custom/exam_theme/js/menu.js
@@ -1,16 +1,25 @@
-(function($) {
-  $(document).ready(function() {
-    // Створення елемента іконки гамбургер-меню
-    var $hamburgerToggle = $('<div class="hamburger-toggle"><span></span><span></span><span></span></div>');
-    $('.block.block-menu.navigation.menu--main').prepend($hamburgerToggle);
+document.addEventListener('DOMContentLoaded', function() {
+  // Створення елемента іконки гамбургер-меню
+  var hamburgerToggle = document.createElement('div');
+  hamburgerToggle.classList.add('hamburger-toggle');
 
-    // Функція для показу/приховування меню
-    function toggleMenu() {
-      $('.menu.menu-level-0').toggleClass('open');
-      $hamburgerToggle.toggleClass('open');
-    }
+  var spans = [];
+  for (var i = 0; i < 3; i++) {
+    var span = document.createElement('span');
+    hamburgerToggle.appendChild(span);
+    spans.push(span);
+  }
 
-    // Обробник події клік на іконку гамбургер-меню
-    $hamburgerToggle.on('click', toggleMenu);
-  });
-})(jQuery);
+  var menuBlock = document.querySelector('.block.block-menu.navigation.menu--main');
+  menuBlock.prepend(hamburgerToggle);
+
+  // Функція для показу/приховування меню
+  function toggleMenu() {
+    var menu = document.querySelector('.menu.menu-level-0');
+    menu.classList.toggle('open');
+    hamburgerToggle.classList.toggle('open');
+  }
+
+  // Обробник події клік на іконку гамбургер-меню
+  hamburgerToggle.addEventListener('click', toggleMenu);
+});
