8fcfd8cfe640c083c6e3489c288f32c7477ef6be
DonutSergius <popov.serg.2003.09@gmail.com>
2024-03-22 10:57:57 +0200

    Fixed like button | Added title taxonomy term page

--- web/themes/custom/exam_theme/js/menu.js
+++ web/themes/custom/exam_theme/js/menu.js
@@ -1,8 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // Створення елемента іконки гамбургер-меню
+// Creating the hamburger menu icon element
   var hamburgerToggle = document.createElement('div');
   hamburgerToggle.classList.add('hamburger-toggle');
 
+// Creating three span elements and appending them to the hamburgerToggle element
   var spans = [];
   for (var i = 0; i < 3; i++) {
     var span = document.createElement('span');
@@ -10,16 +11,19 @@ document.addEventListener('DOMContentLoaded', function() {
     spans.push(span);
   }
 
+// Selecting the menu block element
   var menuBlock = document.querySelector('.block.block-menu.navigation.menu--main');
+
+// Prepending the hamburgerToggle element to the menu block
   menuBlock.prepend(hamburgerToggle);
 
-  // Функція для показу/приховування меню
+// Function to toggle the visibility of the menu
   function toggleMenu() {
     var menu = document.querySelector('.menu.menu-level-0');
     menu.classList.toggle('open');
     hamburgerToggle.classList.toggle('open');
   }
 
-  // Обробник події клік на іконку гамбургер-меню
+// Event listener for click on the hamburger menu icon, invoking toggleMenu function
   hamburgerToggle.addEventListener('click', toggleMenu);
 });
